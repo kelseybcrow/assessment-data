2a8682ffeaa418c7282b87451fed37efa6d830dc
Kelsey Crow <kelseybcrow@gmail.com>
2022-02-06 18:03:44 -0700

    progress

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -247,4 +247,48 @@ module.exports = {
             .then((dbRes) => res.status(200).send(dbRes[0]))
             .catch((err) => console.log(err))
     },
+
+    createCity: (req, res) => {
+        console.log(req.body)
+        const { name, rating, countryId } = req.body
+
+        sequelize
+            .query(
+                `
+                INSERT INTO cities (name, rating, country_id)
+                VALUES ('${name}', ${rating}, ${countryId})
+            `
+            )
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log(err))
+    },
+
+    getCities: (req, res) => {
+        sequelize
+            .query(
+                `
+                SELECT ci.city_id, ci.name AS city, co.country_id, co.name AS country,
+                FROM cities AS ci
+                JOIN countries AS co
+                ON ci.country_id = co.country_id;
+            `
+            )
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log(err))
+    },
+
+    deleteCity: (req, res) => {
+        const { id } = req.params
+
+        sequelize
+            .query(
+                `
+                DELETE city
+                FROM cities
+                WHERE city_id = ${id};
+            `
+            )
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log(err))
+    },
 }
